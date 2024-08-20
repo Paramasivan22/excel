82bcc03710ec67a82eca3ee2a33cb0216d9d7fd5
Paramasivan22 <sivaaudhimoolam@gmail.com>
2024-08-20 12:28:07 +0530

    back

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -147,7 +147,7 @@
             <div class="nav-item">
                 <a href="{{ route('import_excel_post') }}">
                     <i class="fas fa-file-upload"></i>
-                    <div>Upload User</div>
+                    <div>Upload Users</div>
                 </a>
             </div>
             <div class="nav-item">
